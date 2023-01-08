d6a77e18ea3ebd73a48d8d34842a44378b527f93
Roy Moger-Reischer <rmogerr2@gmail.com>
2023-01-08 10:20:43 -0800

    Complete javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+let reverseString = function(inputString = '') {
+    let reversedString="";
+    //let inputStringAsArray=inputString.split("");
+    for (let i = inputString.length; i>0; i-=1) {
+        reversedString+=inputString[i-1];
+    }
 
-};
+    console.log(reversedString)
+    return reversedString;
+}
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
