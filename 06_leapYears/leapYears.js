822280acc96cd2f144eaff5ad94e6206b6337510
Roy Moger-Reischer <rmogerr2@gmail.com>
2023-01-09 21:10:18 -0800

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let isItALeapYear = -9;
+    if (typeof(inputYear) !== "number" && typeof(inputYear)!== "bigint") {
+        isItALeapYear = false;
+    }
+    else if (inputYear%400 === 0) {
+        isItALeapYear = true;
+    }
+    else if (inputYear%100 === 0) {
+        isItALeapYear = false;
+    }
+    else if (inputYear%4 === 0) {
+        isItALeapYear = true;
+    }
+     
+    else {
+        isItALeapYear = false;
+    }
 
+    return isItALeapYear;
 };
 
 // Do not edit below this line
