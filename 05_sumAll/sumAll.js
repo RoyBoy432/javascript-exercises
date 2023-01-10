990786140c42be521aa802cdcea4ba3fcd416542
Roy Moger-Reischer <rmogerr2@gmail.com>
2023-01-09 20:02:47 -0800

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+let sumAll = function(num1, num2) {
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    let sum = 0; let min = Math.min(num1, num2); let max = Math.max(num1, num2);
+    for (let i = min; i<=max; i+=1) {
+        sum+=i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
