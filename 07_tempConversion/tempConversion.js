1311647c705da4b5368b8c58d202d5c41e3154f4
Roy Moger-Reischer <rmogerr2@gmail.com>
2023-01-10 20:47:10 -0800

    Complete exercise 07 (javascript)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+    let degC = Number(((inputTemp-32)*5/9).toFixed(1));
+    return degC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+    let degF = Number(((inputTemp*9/5)+32).toFixed(1));
+    return degF;
 };
 
 // Do not edit below this line
